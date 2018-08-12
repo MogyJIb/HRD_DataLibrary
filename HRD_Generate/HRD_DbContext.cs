ab5cc15b353b6a83565f3a59e5ec29c57373e111
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2018-08-12 20:43:05 +0300

    Add generate project

---
+++ HRD_Generate/HRD_DbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.IO;
+using HRD_DataLibrary.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace HRD_Api.Data
+{
+    public class HRD_DbContext : DbContext
+    {
+        public HRD_DbContext(DbContextOptions<HRD_DbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<FiredEmployee> FiredEmployees { get; set; }
+        public virtual DbSet<Holiday> Holidays { get; set; }
+        public virtual DbSet<Position> Positions { get; set; }
+        public virtual DbSet<Resume> Resumes { get; set; }
+        public virtual DbSet<Reward> Rewards { get; set; }
+        public virtual DbSet<Vacancy> Vacancies { get; set; }
+        public virtual DbSet<WorkedTime> WorkedTimes { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+   
+            var options = optionsBuilder
+                .UseSqlite("DataSource=humanResourcesDepartment.db")
+                .Options;
+        }
+    }
+}
