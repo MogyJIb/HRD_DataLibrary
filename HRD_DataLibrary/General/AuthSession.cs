ac9a5b6066dc51d55b0fe1e4b174c67780f5e6fa
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2018-08-12 22:47:26 +0300

    Add session class

---
+++ HRD_DataLibrary/General/AuthSession.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRD_DataLibrary.General
+{
+    public class AuthSession
+    {
+        public string Id { get; }
+
+        public AuthSession()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}";
+        }
+
+        protected bool Equals(AuthSession other)
+        {
+            return string.Equals(Id, other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((AuthSession) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Id != null ? Id.GetHashCode() : 0);
+        }
+    }
+}
