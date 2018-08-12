04f5425138e6dcc3b34c0811f9eec5e78ef5cc16
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2018-08-12 21:04:39 +0300

    Refactor error type

--- HRD_DataLibrary/Errors/ErrorType.cs
+++ HRD_DataLibrary/Errors/ErrorType.cs
@@ -8,5 +8,34 @@ namespace HRD_DataLibrary.Errors
 {
     class ErrorType
     {
+        public long ErrorCode { get; set; }
+
+        public ErrorType(long errorCode)
+        {
+            ErrorCode = errorCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(ErrorCode)}: {ErrorCode}";
+        }
+
+        protected bool Equals(ErrorType other)
+        {
+            return ErrorCode == other.ErrorCode;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ErrorType) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return ErrorCode.GetHashCode();
+        }
     }
 }
