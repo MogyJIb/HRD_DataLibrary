ae735b509b99b106948aa7a6767831738587a17d
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2018-08-27 02:17:04 +0300

    Add errors enum

--- HRD_DataLibrary/Errors/ErrorType.cs
+++ HRD_DataLibrary/Errors/ErrorType.cs
@@ -6,38 +6,10 @@ using System.Threading.Tasks;
 
 namespace HRD_DataLibrary.Errors
 {
-    public enum ErrorType { WrongPassword = 410, NonExistentLogin = 411 }
-
-    public class Error
+    public enum ErrorType
     {
-        public ErrorType ErrorType { get; }
-
-        public Error(ErrorType errorType)
-        {
-            ErrorType = errorType;
-        }
-
-        public override string ToString()
-        {
-            return $"{nameof(ErrorType)}: {ErrorType}";
-        }
-
-        protected bool Equals(Error other)
-        {
-            return ErrorType == other.ErrorType;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((ErrorType) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return ErrorType.GetHashCode();
-        }
+        WrongPassword = 410,
+        NonExistentLogin = 411,
+        AuthanticationFaild = 412
     }
 }
